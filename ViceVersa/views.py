98f5d4af89a77b1fe93d937522c4433f833ecf65
Martin <martin.de.haart@gmail.com>
2021-11-28 22:18:25 +0200

    it's working!'

--- ViceVersa/views.py
+++ ViceVersa/views.py
@@ -7,3 +7,11 @@ def about(request):
 
 def home(request):
 	return render(request, 'home.html', {'greetiing':'Hello'})
+
+def reverse(request):
+	test_get_text = request.GET['usertext']
+	newstring = test_get_text[::-1]
+
+	print(newstring)
+	return render(request, 'reverse.html', {'new_user_text':newstring, 'old_user_text':test_get_text})	
+
